c7bfa33e38516bdd0eab0ff11ab3f110ce7c76cd
AracenaSC <aracenacristopher@gmail.com>
2022-02-22 14:58:17 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './views/about/about.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { SheetComponent } from './views/sheet/sheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SheetComponent},
+  {path: '/sheet', component: SheetComponent},
+  {path: '/about', component: AboutComponent},
+  {path: '/login', component: LoginComponent},
+  {path: '/register', component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SheetComponent } from './views/sheet/sheet.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './views/about/about.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SheetComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
